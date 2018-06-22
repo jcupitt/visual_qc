b92a4fed2ea59569ff08b2daa8bc64eb7f83fe05
John Cupitt <jcupitt@gmail.com>
2018-06-22 17:45:12 +0100

    seed the db, add scan show

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+User.create!(name: "John C")
+
+# the participants.tsv file we load
+participants_tsv = "db/qc_participants.tsv"
+
+all_scans = {}
+CSV.foreach(participants_tsv, col_sep: "\t") do |row|
+	subject = row[0]
+	session = row[1]
+	gender = row[2]
+	birth_ga = row[3]
+
+  # header line
+  next if subject == "participant_id"
+
+  # duplicate
+  next if all_scans.has_key? subject
+
+  all_scans[subject] = {
+      subject: subject,
+      gender: gender,
+      birth_ga: birth_ga
+  }
+
+  Scan.create!(subject: subject, session: session)
+
+end
